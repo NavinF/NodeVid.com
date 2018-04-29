0b8513192e6f39f4460ac41b69798c9a4aa5bfc2
Navin Francis <navin@altiora.us>
2018-04-29 09:11:58 -0700

    rm //atube and use //website instead

--- deploy.py
+++ deploy.py
@@ -2,7 +2,7 @@
 import subprocess
 import deploy_keys
 
-subprocess.run(['docker','build','-t','atube_webserver','-f','atube/Dockerfile','.'])
+subprocess.run(['docker','build','-t','atube_webserver','-f','website/Dockerfile','.'])
 subprocess.run(deploy_keys.docker_login_cmd)
 subprocess.run(['docker', 'tag', 'atube_webserver:latest', '355023123875.dkr.ecr.us-east-1.amazonaws.com/atube_webserver:latest'])
 subprocess.run(['docker', 'push', '355023123875.dkr.ecr.us-east-1.amazonaws.com/atube_webserver:latest'])
